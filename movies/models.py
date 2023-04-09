ea57c8073fcfd0e8d6bb55901db446c7f8378ca8
dmadhav <manav4consultsu@gmail.com>
2023-04-09 20:18:27 +0545

    Skeleton created

---
+++ movies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+# app/movies/models.py
+
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    pass
+
+# Create your models here.
